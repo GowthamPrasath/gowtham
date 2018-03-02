3e8083966d476346f657d096703c9f0f1382f635
R Gowtham prasath <35652739+GowthamPrasath@users.noreply.github.com>
2018-03-02 14:44:37 +0530

    Update number or char.c

--- number or char.c
+++ number or char.c
@@ -5,7 +5,7 @@ void main()
     printf("Enter the string");
     scanf("%s",&a);
     if(a>=65 && a<=90 || a>=97 && a<=122)
-    printf("not a number");
+    printf("No");
     else
     {
         printf("yes");
