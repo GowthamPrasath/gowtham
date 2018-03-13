d851289bb28499a94b033aa0910529b02d2e818f
R Gowtham prasath <35652739+GowthamPrasath@users.noreply.github.com>
2018-03-13 16:38:42 +0530

    Create 46printnxtnum.c

---
+++ 46printnxtnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i;
+    clrscr();
+    printf("Enter the numbers");
+    scanf("%d",&a);
+    {
+        for(i=a;i<=a;i++)
+        b=i+1;
+    }
+    printf("%d",b);
+    getch();
+}
